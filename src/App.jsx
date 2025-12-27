e0637c570a89d7f5fb4350cc904712f2fa211a20
nkfz56dk27-byte <nkfz56dk27@privaterelay.appleid.com>
2025-12-27 12:01:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1959,8 +1959,7 @@ function HomeView({ user, onLogout, onOpenGestione, onOpenClassificheMenu, onOpe
                 <path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 16H8v-2h8v2zm0-4H8v-2h8v2zm-3-5V3.5L18.5 9H13z"/>
               </svg>
             </div>
-            <h3 className="card-title">PANNELLO FONTI</h3>
-            <p className="card-subtitle">Fonti<br />articoli F1</p>
+            <h3 className="card-title">PANNELLO FONTI F1</h3>
           </div>
         </div>
       </div>
