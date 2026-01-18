c1d32f2ec28b3bb890461c74d54054505e25522f
nkfz56dk27-byte <nkfz56dk27@privaterelay.appleid.com>
2026-01-18 22:39:07 +0100

    fix: usa OneSignal per /api/send-notification

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const app = express();
 const sendOneSignalApi = require('./send-onesignal-api');
 
 app.use(express.json());
-app.use('/api/send-notification', require('./send-fcm-notification'));
+// app.use('/api/send-notification', require('./send-fcm-notification'));
 app.use('/api', sendOneSignalApi);
 
 const PORT = process.env.PORT || 3000;
