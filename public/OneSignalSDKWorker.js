d608454b33461f54d2578f95eabb7780c1445577
nkfz56dk27-byte <nkfz56dk27@privaterelay.appleid.com>
2026-02-12 23:56:24 +0100

    fix: aggiunto handler message vuoto per warning Service Worker

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,1 +1,5 @@
+// Handler vuoto per soddisfare i requisiti del browser
+self.addEventListener('message', function(event) {
+	// Puoi gestire i messaggi qui se necessario
+});
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
